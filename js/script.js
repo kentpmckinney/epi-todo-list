c69eb10ee0fc27506f195637d3df2ff352dc2d21
Kent McKinney <kent.p.mckinney@gmail.com>
2020-02-09 14:34:00 -0800

    implement adding item to checklist

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function ToDoList() {
+  this.list = [];
+}
+
+function ToDoItem(description) {
+  this.description = description;
+  this.order = 0;
+}
+
+ToDoList.prototype.add = function(description) {
+  let newItem = new ToDoItem(description);
+  this.list.push(newItem);
+}
+
+ToDoList.prototype.remove = function(description) {
+  return null;
+}
+
+$(document).ready(function(){
+
+  $("#form").submit(function(e) {
+    let description = $("#description").val();
+    $("#result").append(`<input type="checkbox">${description}</div>`);
+    e.preventDefault();
+  });
+
+});
+
